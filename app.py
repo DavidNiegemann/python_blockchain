84cf7455b70851fcefe7a73e67022296df65c1b2
David.NIEGEMANN <David.NIEGEMANN@neel.cnrs.fr>
2021-04-04 16:00:53 +0200

    SQL app

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from passlib.hash import sha256_crypt
-from flask_mysqldb import MySQL
-
-from passwords import _mysql_password
-
-app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_VARIABLE'] = _mysql_password
-app.config['MYSQL_DB'] = 'crypto'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
-
-
-@app.route("/")
-def index():
-    return 'Hello world!'
-
-
-if __name__ == '__main__':
-    app.secret_key = 'secret123'
-    app.run(debu=True)
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+
+from passwords import _mysql_password
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_VARIABLE'] = _mysql_password
+app.config['MYSQL_DB'] = 'crypto'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'secret123'
+    app.run(debug=True)
