3284181d66d25078a189fc2b3eb67d3660d7f8b6
David.NIEGEMANN <David.NIEGEMANN@neel.cnrs.fr>
2021-04-10 18:09:53 +0200

    Update of html interface

--- app.py
+++ app.py
@@ -1,29 +1,107 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
+from functools import wraps
 
 from passwords import _mysql_password
 from sqlhelpers import *
+from forms import *
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_VARIABLE'] = _mysql_password
+app.config['MYSQL_PASSWORD'] = _mysql_password
 app.config['MYSQL_DB'] = 'crypto'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Unauthorized, please login", "danger")
+            return redirect(url_for('login'))
+        return wrap
+
+
+def log_in_user(username):
+    users = Table("users", "name", "email", "username", "password")
+    user = users.getone("username", username)
+
+    session['logged_in'] = True
+    session['username'] = username
+    seesion['name'] = user.get('name')
+    session['email'] = user.get('email')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    users = Table("users", "name", "email", "username", "password")
+
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        name = form.name.data
+
+        if isnewuser(username):  # Check if new user
+            password = sha256_crypt.encrypt(form.password.data)
+            user.insert(name, email, username, password)
+            log_in_user(username)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('User already exists', 'danger')
+            return redirect(url_for('register'))
+
+    return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        candidate = request.form['password']
+
+        users = Table("users", "name", "email", "username", "password")
+        user = users.getone("username", username)
+        accpass = user.get('password')
+
+        if accPass is None:
+            flash("Username is not found", 'danger')
+            return redirect(url_for('login'))
+        else:
+            if sha256_crypt.verify(candidate, accpass):
+                log_in_user(username)
+                flash('You are now logged in.', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Invalid password", 'danger')
+                return redirect(url_for('login'))
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("Logout success", "success")
+    return redirect(url_for('login'))
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html', session=session)
+
+
 @app.route("/")
 def index():
-    users = Table("users","name","email","username","password")
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
     app.run(debug=True)
-
-
-    
